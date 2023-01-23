30914213218d67a20670fb6fea5f6dfb99a09c0a
SanthoshiBoyina <83487694+SanthoshiBoyina@users.noreply.github.com>
2023-01-23 09:59:19 +0530

    Update Blogs.js

--- Frontend/src/components/Blogs.js
+++ Frontend/src/components/Blogs.js
@@ -16,7 +16,6 @@ const Blogs = () => {
   useEffect(()=> {
     sendRequest().then((data) => setBlogs(data.blogs));
   }, [])
-  // console.log(blogs);
   return <div>
     {blogs &&
       blogs.map((blog, index) => (
@@ -25,7 +24,7 @@ const Blogs = () => {
         isUser={localStorage.getItem("userId") === blog.user._id}
         title={blog.title}
         description={blog.description}
-        imageURL={blog.image}
+        image={blog.image}
         userName={blog.user.name}
       />
     ))}
@@ -36,38 +35,3 @@ export default Blogs
 
 
 
-// import React, { useEffect, useState } from "react";
-// import axios from "axios";
-// import Blog from "./Blog";
-
-// const Blogs = () => {
-//   const [blogs, setBlogs] = useState();
-//   const sendRequest = async () => {
-//     const res = await axios
-//       .get("http://localhost:5000/api/blog")
-//       .catch((err) => console.log(err));
-//     const data = await res.data;
-//     return data;
-//   };
-//   useEffect(() => {
-//     sendRequest().then((data) => setBlogs(data.blogs));
-//   }, []);
-//   console.log(blogs);
-//   return (
-//     <div>
-//       {blogs &&
-//         blogs.map((blog, index) => (
-//           <Blog
-//             id={blog._id}
-//             isUser={localStorage.getItem("userId") === blog.user._id}
-//             title={blog.title}
-//             description={blog.description}
-//             imageURL={blog.image}
-//             userName={blog.user.name}
-//           />
-//         ))}
-//     </div>
-//   );
-// };
-
-// export default Blogs;
