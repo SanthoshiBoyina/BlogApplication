75287410bef727b77868ef3489e5f909d363f3e0
SanthoshiBoyina <83487694+SanthoshiBoyina@users.noreply.github.com>
2023-01-23 10:00:15 +0530

    Update UserBlogs.js

--- Frontend/src/components/UserBlogs.js
+++ Frontend/src/components/UserBlogs.js
@@ -1,20 +1,7 @@
-// import React from 'react'
-
-// const UserBlogs = () => {
-//   return (
-//     <div>UserBlogs</div>
-//   )
-// }
-
-// export default UserBlogs
-
-
-
-
-
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Blog from "./Blog";
+
 const UserBlogs = () => {
   const [user, setUser] = useState();
   const id = localStorage.getItem("userId");
@@ -43,7 +30,7 @@ const UserBlogs = () => {
             isUser={true}
             title={blog.title}
             description={blog.description}
-            imageURL={blog.image}
+            image={blog.image}
             userName={blog.user.name}
           />
         ))}
@@ -57,41 +44,3 @@ export default UserBlogs;
 
 
 
-// import React, { useEffect, useState } from "react";
-// import axios from "axios";
-// import Blog from "./Blog";
-// const UserBlogs = () => {
-//   const [user, setUser] = useState();
-//   const id = localStorage.getItem("userId");
-//   const sendRequest = async () => {
-//     const res = await axios
-//       .get(`http://localhost:5000/api/blog/user/${id}`)
-//       .catch((err) => console.log(err));
-//     const data = await res.data;
-//     return data;
-//   };
-//   useEffect(() => {
-//     sendRequest().then((data) => setUser(data.user));
-//   }, []);
-//   console.log(user);
-//   return (
-//     <div>
-//       {" "}
-//       {user &&
-//         user.blogs &&
-//         user.blogs.map((blog, index) => (
-//           <Blog
-//             id={blog._id}
-//             key={index}
-//             isUser={true}
-//             title={blog.title}
-//             description={blog.description}
-//             imageURL={blog.image}
-//             userName={user.name}
-//           />
-//         ))}
-//     </div>
-//   );
-// };
-
-// export default UserBlogs;
